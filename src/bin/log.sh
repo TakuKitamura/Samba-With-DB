478c1a0232e6866008e194d41ae76c21938469c9
Taku Kitamura <tikutaku0128@icloud.com>
2017-12-27 20:14:35 +0900

    大幅な修正

---
+++ src/bin/log.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# 外部変数読み込み
+. ../sql/createTable/db.conf
+
+log() {
+  tableName=$1
+  createFileUserName=$2
+  updatedAt=$3
+  operation=$4
+  filePath=$5
+
+  fileName=`basename filePath`
+
+  createTableSQL=../sql/createTable/$tableName.sql
+  createdTableSQL=../sql/createTable/${tableName}_created.sql
+
+  sha256=`shasum -a 256 $filePath | cut -c 1-64`
+
+  if [ ! -f $createTableSQL ]; then
+    psql -U $USER -h $HOST -d $DBNAME < createTableSQL
+    mv createTableSQL ../sql/createTable/${tableName}_created.sql
+  fi
+
+  if [ ! -f $createdTableSQL ]; then
+    cat $createdTableSQL
+    echo
+  fi
+
+  psql psql -U $USER -h $HOST -d $DBNAME -c " \
+  INSERT INTO $tableName \
+    (create_file_user_name, file_name, file_path, sha256, created_at, updated_at)
+      VALUES ($createFileUserName, $fileName, $filePath, $sha256, $updatedAt, $updatedAt)
+  "
+}
---
+++ start.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+tableName=$1
+user=ri-one
+host=192.168.11.5
+
+outsideFunctionAbsolutePath=`readlink -f ./src/bin/$tableName.sh`
+
+# 外部ファイル読み込み
+. $outsideFunctionAbsolutePath
+
+# syslog 絶対パス
+sysLogPath='/var/log/syslog'
+
+# syslog のシンボリックリンクのファイルパス
+syslogSymbolicLink='./syslog'
+if [ ! -f $syslogSymbolicLink ]; then
+  # 既にsyslog のシンボリックリンクが存在していなかったら、念のためシンボリックを作成
+  ln -s $sysLogPath $syslogSymbolicLink
+fi
+
+parseSambaLog() {
+  while IFS=' \t\n' read line
+  do
+    if [[ $line =~ smbd_audit: && ! $line =~ \|ok\|\.|.+\/\. ]] ;
+    then
+      echo $line | \
+      while IFS='|' read header netBIOSName topDirecoryPath updatedTime operation operationResult operatedPath; do
+        echo netBIOSName, $netBIOSName
+        echo updatedTime, $updatedTime
+        echo operation, $operation
+        operationAbsolutePath=$topDirecoryPath/$operatedPath
+        echo operationAbsolutePath, $operationAbsolutePath
+        echo
+
+        IFS=''
+
+        if [ ! -f $syslogSymbolicLink ]; then
+          # 既にsyslog のシンボリックリンクが存在していなかったら、念のためシンボリックを作成
+          ln -s $sysLogPath $syslogSymbolicLink
+        fi
+
+        # 外部関数呼び出し
+        $outsideFunctionAbsolutePath $tableName \
+          $netBIOSName $updatedTime $operation $operationAbsolutePath
+      done
+    fi
+  done
+}
+echo $syslogSymbolicLink
+sudo tail --follow=name --retry -n 0 $syslogSymbolicLink | parseSambaLog
