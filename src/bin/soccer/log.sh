799a00bae2a0fadf28e5e39144c9dcb5cb55ae89
Taku Kitamura <tikutaku0128@icloud.com>
2017-12-28 12:38:09 +0900

    リファクタ

--- src/bin/soccer/log.sh
+++ src/bin/soccer/log.sh
@@ -7,16 +7,18 @@ log() {
   # ex. /home/hoge/keep-watch-on-samba
   gitRootDirectoryPath=$1
 
+  dataBaseName=$2
+
   # 外部変数読み込み
   # 例えば、以下の変数が利用できるようになる
   # HOST=192.168.11.5
-  # DBNAME=soccer
+  # dataBaseName=soccer
   # USER=ri-one
-  . $gitRootDirectoryPath/src/conf/db.conf
+  . $gitRootDirectoryPath/sql/$dataBaseName/db.conf
 
-  # log 関数へ渡された第七引数
+  # log 関数へ渡された第八引数
   # ex. /home/hoge/share/hoge/test.txt
-  filePath=$7
+  filePath=$8
 
   # shasum -a 256 $filePath で、sha256 を生成
   # $shasum -a 256 hoge.txt
@@ -25,29 +27,32 @@ log() {
   # 1文字目から、64文字目(sha256は64文字)までを $sha256 に格納
   sha256=`shasum -a 256 $filePath | cut -c 1-64`
 
-  # log 関数へ渡された第二引数
+  # log 関数へ渡された第三引数
   # ex. user
-  tableName=$2
+  tableName=$3
 
   # テーブルを作成するための sqlファイル へのパス
-  # ex. /home/hoge/keep-watch-on-samba/src/sql/createTable/user.sql
-  createTableSQL=$gitRootDirectoryPath/src/sql/createTable/$tableName.sql
+  # ex. /home/hoge/keep-watch-on-samba/src/sql/$dataBaseName/user.sql
+  createTableSQL=$gitRootDirectoryPath/src/sql/$dataBaseName/$tableName.sql
+
+
+  # テーブルを作成後のsqlファイル へのパス
+  # ex. /home/hoge/keep-watch-on-samba/src/sql/$dataBaseName/user_created.sql
+  createdTableSQL=$gitRootDirectoryPath/src/sql/$dataBaseName/${tableName}_created.sql
 
   # $createTableSQL のパスが存在するとき true
   # true のときテーブルを作成する
   if [ -f $createTableSQL ]; then
 
-    # ex. psql -U ri-one -h 192.168.11.5 -d soccer < /home/ri-one/keep-watch-on-samba/src/sql/createTable/user.sql
+    # ex. psql -U ri-one -h 192.168.11.5 -d soccer < /home/ri-one/keep-watch-on-samba/src/sql/$dataBaseName/user.sql
     # psql についての詳細は → Google!
-    psql -U $USER -h $HOST -d $DBNAME < $createTableSQL
+    psql -U $USER -h $HOST -d $dataBaseName < $createTableSQL
 
-    # /home/hoge/keep-watch-on-samba/src/sql/createTable/user.sql から
-    # へファイル名変更 /home/hoge/keep-watch-on-samba/src/sql/createTable/user_created.sql
-    mv $createTableSQL $gitRootDirectoryPath/src/sql/createTable/${tableName}_created.sql
+    # /home/hoge/keep-watch-on-samba/src/sql/$dataBaseName/user.sql から
+    # へファイル名変更 /home/hoge/keep-watch-on-samba/src/sql/$dataBaseName/user_created.sql
+    mv $createTableSQL $createdTableSQL
   fi
 
-  # ex. /home/hoge/keep-watch-on-samba/src/sql/createTable/user_created.sql
-  createdTableSQL=$gitRootDirectoryPath/src/sql/createTable/${tableName}_created.sql
 
   # $createdTableSQL のパスが存在するとき true
   # true のとき、既にテーブルが作成されているので、テーブルを作成したsqlファイルを標準出力
@@ -57,9 +62,9 @@ log() {
     echo
   fi
 
-  # log 関数へ渡された第五引数
+  # log 関数へ渡された第六引数
   # ex. pwrite
-  operation=$5
+  operation=$6
 
   # C言語的には SWITCH文
   case $operation in
@@ -67,22 +72,22 @@ log() {
     # $operation が pwriteのときに行われたファイル操作は新規作成
     "pwrite" )
 
-      # log 関数へ渡された第三引数
+      # log 関数へ渡された第四引数
       # ex. hogetaro
-      createFileUserName=$3
+      createFileUserName=$4
 
       # クライアントが操作を行ったファイル名
       # ex. $ basename /home/hoge/share/hoge/test.txt
       #       test.txt
       fileName=`basename $filePath`
 
-      # log 関数へ渡された第四引数
+      # log 関数へ渡された第五引数
       # ex. 2017/12/27 11:05:00
-      updatedAt=$4
+      updatedAt=$5
 
       # psql についての詳細は → Google!
       # SQL についての詳細は → Google!
-      psql -U $USER -h $HOST -d $DBNAME -c " \
+      psql -U $USER -h $HOST -d $dataBaseName -c " \
       INSERT INTO $tableName \
         (create_file_user_name, file_name, file_path, sha256, created_at, updated_at) \
           VALUES ('$createFileUserName', '$fileName', '$filePath', '$sha256', '$updatedAt', '$updatedAt');
@@ -92,7 +97,7 @@ log() {
     "unlink" )
       # psql についての詳細は → Google!
       # SQL についての詳細は → Google!
-      psql -U $USER -h $HOST -d $DBNAME -c " \
+      psql -U $USER -h $HOST -d $dataBaseName -c " \
       DELETE FROM $tableName WHERE file_path = '$filePath';
     " ;;
 
@@ -104,13 +109,13 @@ log() {
       # ex. 2017/12/27 11:05:00
       updatedAt=`date +"%Y/%m/%d %I:%M:%S"`
 
-      # log 関数へ渡された第六引数
+      # log 関数へ渡された第七引数
       # ex. /home/hoge/share/hoge/test.txt
-      beforeFilePath=$6
+      beforeFilePath=$7
 
       # psql についての詳細は → Google!
       # SQL についての詳細は → Google!
-      psql -U $USER -h $HOST -d $DBNAME -c " \
+      psql -U $USER -h $HOST -d $dataBaseName -c " \
         UPDATE $tableName SET file_name='$fileName', file_Path='$filePath', updated_at='$updatedAt' \
           WHERE file_path='$beforeFilePath' AND sha256 = '$sha256';
     " ;;
