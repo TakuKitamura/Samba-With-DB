1f37be812ff47f56a3b401503e8334aacf8fb23c
Taku Kitamura <tikutaku0128@icloud.com>
2018-02-03 10:42:33 +0900

    key file_nameを削除

--- src/sql/soccer/log.sql
+++ src/sql/soccer/log.sql
@@ -1,10 +1,9 @@
 CREATE TABLE log
 (
-	  file_path text  NOT NULL,
-	  sha256 text  NOT NULL,
-	  create_file_user_name text NOT NULL,
-	  file_name text  NOT NULL,
-	  created_at timestamp with time zone NOT NULL,
-	  updated_at timestamp with time zone NOT NULL,
-	  PRIMARY KEY (file_path)
+  file_path text  NOT NULL,
+  create_file_user_name text NOT NULL,
+  created_at timestamp with time zone NOT NULL,
+  updated_at timestamp with time zone NOT NULL,
+  sha256 text  NOT NULL,
+  PRIMARY KEY (file_path)
 );
