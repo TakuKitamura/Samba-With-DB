79cb1446cd01e93f7a01edfdd1e9f482c8ba13e4
Your Name <you@example.com>
2018-02-09 23:00:55 +0900

    test.shを意味のあるものに修正

--- tests/test.sh
+++ tests/test.sh
@@ -2,9 +2,9 @@
 
 testPermission()
 {
-	echo "Feb 10 12:19:47 debian smbd_audit: |kitamurataku|/home/ri-one/share|2018/02/10 12:19:47|pwrite|ng|test.pdf" | . ~/samba-with-db/parse.sh
+	echo "Feb 10 12:19:47 debian smbd_audit: |kitamurataku|/home/ri-one/share|2018/02/10 12:19:47|pwrite|ng|test.pdf" | . ../parse.sh -t
+	ret=$?
+	assertEquals $ret 1
 }
-
-
 
 . ./shunit2-2.1.6/src/shunit2
