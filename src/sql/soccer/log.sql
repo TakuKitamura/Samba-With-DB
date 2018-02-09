1b3d294522addc6cd6158c02c0bf0e1edbb82b4c
yusuke <is0375xs@ed.ritsumei.ac.jp>
2018-02-09 19:12:43 +0900

    修正

--- src/sql/soccer/log.sql
+++ src/sql/soccer/log.sql
@@ -1,9 +1,8 @@
 CREATE TABLE log
 (
-  file_path text  NOT NULL,
-  create_file_user_name text NOT NULL,
-  created_at timestamp with time zone NOT NULL,
-  updated_at timestamp with time zone NOT NULL,
-  sha256 text  NOT NULL,
-  PRIMARY KEY (file_path)
+  id serial  NOT NULL,
+  os text  NOT NULL,
+  user_name text  NOT NULL,
+  global_ip text  NOT NULL,
+  PRIMARY KEY (id)
 );
--- src/sql/soccer/user.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user
-(
-  id serial  NOT NULL,
-  os text  NOT NULL,
-  name text  NOT NULL,
-  global_ip text  NOT NULL,
-  PRIMARY KEY (id)
-);
