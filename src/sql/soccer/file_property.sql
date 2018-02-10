49cc08d7f11ddd7f57b395c49c923cc9e8c3d02f
喜多村卓 <is0352pp@ed.ritsumei.ac.jp>
2018-02-10 11:54:56 +0900

    SQLの順序を更新

--- src/sql/soccer/file_property.sql
+++ src/sql/soccer/file_property.sql
@@ -3,10 +3,10 @@ CREATE TABLE file_property
   absolute_path text NOT NULL,
   created_at timestamp with time zone NOT NULL,
   updated_at timestamp with time zone NOT NULL,
-  user_id integer  NOT NULL,
+  sha256 text  NOT NULL,
   byte_size bigint  NOT NULL,
   author_name text  NOT NULL,
+  user_id integer  NOT NULL,
   league_id integer  NOT NULL,
-  sha256 text  NOT NULL,
   PRIMARY KEY (absolute_path)
 );
--- src/sql/soccer/log.sql
+++ src/sql/soccer/log.sql
@@ -1,8 +1,8 @@
 CREATE TABLE log
 (
   id serial  NOT NULL,
-  os text  NOT NULL,
   user_name text  NOT NULL,
+  os text  NOT NULL,
   global_ip text  NOT NULL,
   PRIMARY KEY (id)
 );
